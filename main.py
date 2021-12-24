3c959ddf82f1828f884adc9ea02438f820b5b673
aniketbhatia9 <66548897+aniketbhatia9@users.noreply.github.com>
2021-12-24 17:48:27 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import speech_recognition as sr
+import pyttsx3
+
+listener = sr.Recognizer()
+engine=pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice',voices[0].id)
+
+
+def talk(text):
+    engine.say('I am your alexa')
+    engine.say('What I can do for you')
+    engine.runAndWait()
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
+                print(command)
+    except:
+        pass
+    return command
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        talk('playing')
+        print('playing ')
+
+run_alexa()
